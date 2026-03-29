a0fb92c50c188d723fe7e1e8bd6d1e621407e624
nyutxxrws <nyutxxrws@gmail.com>
2026-03-29 13:23:08 +0300

    task2.py

--- task2.py
+++ task2.py
@@ -6,8 +6,9 @@ def is_palindrome(s):
     else:
         return False
 assert is_palindrome("Лёша на полке клопа нашёл") == True
+assert is_palindrome("Коту тащат уток") == True
 assert is_palindrome("А роза упала на лапу Азора") == True
 assert is_palindrome("Казак") == True
 assert is_palindrome("Мир") == False
 assert is_palindrome("12321") == True
-print("Все 5 тестов пройдены успешно!")
+print("Все 6 тестов пройдены успешно!")
